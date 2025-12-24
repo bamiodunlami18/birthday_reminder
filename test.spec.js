250caa90bb4ed66104fdc9fd7c76871e9cc453dd
bamiodunlami18 <bamiodunlami18@gmail.com>
2025-12-24 18:32:57 +0000

    first commit

--- test.spec.js
+++ test.spec.js
@@ -1,6 +1,9 @@
 const { createDate } = require('./models/date.model');
+const jest = require('jest')
+
+jest.mock('mongoose')
 
 it('should return string of date created', async () => {
   const saveDate = await createDate;
-  expect(saveDate).toBeDefined();
+  expect(saveDate).toBeDefined()
 });
